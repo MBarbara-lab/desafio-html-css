2a52733c9d290e7bbd03ff99f75715ec91feef2a
MBarbara-lab <melissa.cursos.me@gmail.com>
2025-03-10 11:59:15 -0300

    responsive

---
+++ app.js
@@ -0,0 +1,7 @@
+function showPopUp () {
+    if (socialMediaPopUp.style.display == "flex") {
+        socialMediaPopUp.style.display = "none"
+    } else {
+        socialMediaPopUp.style.display = "flex"
+    }
+}
